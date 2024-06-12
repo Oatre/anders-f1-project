ed13e1375307290273c9ba2a254b6670674aca43
Oatre <jensenandfur@hotmail.com>
2024-06-12 15:48:13 +0200

    Commiting new changes, made changes to the bingotile, they will be rendered through bingogrid. Still working on bingogrid.

--- src/Components/BingoTile/BingoTile.jsx
+++ src/Components/BingoTile/BingoTile.jsx
@@ -2,20 +2,46 @@ import React, { useState } from "react";
 import "./bingotile.scss";
 
 const BingoTile = () => {
-    const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const [isInputFinal, setIsInputFinal] = useState(false);
 
-    const handleInputChange = (event) => {
-        setInputValue(event.target.value);
-    };
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+  // This event finalizes the input. If input field is selected, and unfocuses, regardless if anything is typed
+  // the input field is finalized, and you cant type anything
+  const handleInputBlur = () => {
+    if (inputValue === "") {
+      setIsInputFinal(false);
+    } else {
+      setIsInputFinal(true);
+    }
+  };
 
-    return (
-        <div className="BingoTile">
-           <input
-                type="text"
-                value={inputValue}
-                onChange={handleInputChange}
-                placeholder="yoyo" />
-        </div>
-    );
+  //This function makes sure that the user can also press Enter (Keycode 13) to finalize the input field
+  const handleEnterKey = (event) => {
+    if (inputValue === "") {
+      setIsInputFinal(false);
+    } else if (event.keyCode === 13) {
+      setIsInputFinal(true);
+    }
+  };
+
+  return (
+    <div className="BingoTile">
+      {isInputFinal ? (
+        <p>{inputValue}</p>
+      ) : (
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          onBlur={handleInputBlur}
+          onKeyDown={handleEnterKey}
+          placeholder="Fill card"
+        /> // Want to fix that fill and card are on seperate lines later, for now leave as is
+      )}
+    </div>
+  );
 };
 export default BingoTile;
